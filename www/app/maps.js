72977070e4622c34ebd08eb9b51395c7a0188cd6
Iulia Bejan <iubejan@microsoft.com>
2016-10-21 12:13:55 +0200

    font size update

--- www/app/maps.js
+++ www/app/maps.js
@@ -1,16 +1,30 @@
 define(function () {
 	let map;
+	let marker;
+
+	let getMarker = function (coords) {
+		return new google.maps.Marker({
+          position: coords,
+          map: map,
+          title: 'Hello World!'
+        });
+	}
 
 	let initMaps = function() {
-		var berlin = {lat: 52.5200066, lng: 13.404954};
+		var berlin = { lat: 52.5200066, lng: 13.404954 };
 		map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 6,
+			zoom: 7,
 			center: berlin
 		});
+
+		marker = getMarker(berlin);
 	}
 
 	let moveMapTo = function(lat, long) {
-		map.panTo( {lat: lat, lng: long});
+		let coords = {lat: lat, lng: long};
+		marker.setMap(null);
+		marker = getMarker( coords );
+		map.panTo( coords );
 	}
 
     return {
